2f9ff4b6cdd655e07d76d123e98ad918721a1793
ParthPanchal26 <parthpanchal2625@gmail.com>
2024-12-27 11:16:05 +0530

    Configured redux store and authSlice

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
+import React, { useEffect } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth'
+import { login, logout } from './features/authSlice/authSlice';
+import { Header, Footer } from '../src/components/index'
+import { Outlet } from 'react-router-dom';
 
 function App() {
 
-  return (
-    <>
-      <h1>App</h1>
-    </>
-  )
+  const [loading, setLoading] = React.useState(true);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) dispatch(login({ userData }));
+        else dispatch(logout());
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+    // 4:50
+
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-slate-600">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
