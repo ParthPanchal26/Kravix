417a6c586305a5942ef588b0445c4a96ef3d8764
ParthPanchal26 <parthpanchal2625@gmail.com>
2024-12-29 17:15:30 +0530

    Added tinemce API_KEY and made editor function properly

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,20 +1,22 @@
 import { Editor } from '@tinymce/tinymce-react'
 import { Controller } from 'react-hook-form'
+import conf from '../conf/conf'
 
 export default function RTE({ name, control, label, defaultValue }) {
     return (
         <div className="w-full">
             {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
-            
+
             <Controller
                 name={name || 'content'}
                 control={control}
-                defaultValue={defaultValue}
-                render={({field: {onChange, value}}) => (
+                render={({ field: { onChange } }) => (
                     <Editor
-                        value={value}
+                        apiKey={conf.tinymceApiKye}
                         initialValue={defaultValue}
-                        onEditorChange={onChange}
+                        onEditorChange={(newValue) => {
+                            onChange(newValue)
+                        }}
                         init={{
                             initialValue: defaultValue,
                             height: 500,
--- src/conf/conf.js
+++ src/conf/conf.js
@@ -4,6 +4,7 @@ const conf = {
     appWriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
     appWriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appWriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    tinymceApiKye: String(import.meta.env.VITE_TINYMCE_APIKYE)
 };
 
 export default conf;
