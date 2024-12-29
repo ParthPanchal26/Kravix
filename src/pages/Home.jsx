d5ddac4545c62d6d34df4cc022b112d504c06c62
ParthPanchal26 <parthpanchal2625@gmail.com>
2024-12-29 17:13:45 +0530

    Corrected method call and introduced better conditional rendering

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,24 @@
 import { useState, useEffect } from 'react'
 import appwriteService from '../appwrite/config'
 import { Container, PostCard } from '../components/index'
+import { useSelector } from 'react-redux';
 
 const Home = () => {
 
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        appwriteService.getPost().then((posts) => {
+        appwriteService.listPosts().then((posts) => {
             if (posts) {
-                setPosts(posts.document)
+                setPosts(posts.documents)
             }
         })
     }, [])
 
+    const userData = useSelector((state) => state.auth.userData);
 
-    if (posts.length === 0) {
+
+    if (!userData) {
         return (
             <div className="w-full py-8 mt-4 text-center">
                 <Container>
@@ -23,6 +26,20 @@ const Home = () => {
                         <div className="p-2 w-full">
                             <h1 className="text-2xl font-bold hover:text-gray-500">
                                 Login to read posts
+                            </h1>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        )
+    } else if(posts.length === 0) {
+        return (
+            <div className="w-full py-8 mt-4 text-center">
+                <Container>
+                    <div className="flex flex-wrap">
+                        <div className="p-2 w-full">
+                            <h1 className="text-2xl font-bold hover:text-gray-500">
+                                No Record Found
                             </h1>
                         </div>
                     </div>
